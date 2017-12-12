5b3103297762e457d03dd8376d4823211284a785
TinTin <32924832+20twu@users.noreply.github.com>
2017-12-12 18:33:35 +0100

    Poop

--- LifeThingy.playground/Contents.swift
+++ LifeThingy.playground/Contents.swift
@@ -5,7 +5,7 @@ import UIKit
 class life {
     
     var intelligence = 0
-    var money = 0
+    var money: UInt32 = 0
     var addiction = false
     var living = false
     var death:UInt32 = 0
@@ -14,13 +14,13 @@ class life {
         let parentLevel:UInt32 = arc4random_uniform(5) + 1
         
         if parentLevel == 5{
-            money += 20000
+            money += arc4random_uniform(40000) + 20000
         }else if parentLevel == 4{
-            money += 10000
+            money += arc4random_uniform(30000) + 10000
         }else if parentLevel == 2{
-            money -= 10000
+            money -= arc4random_uniform(30000) + 10000
         }else if parentLevel == 1{
-            money -= 20000
+            money -= arc4random_uniform(40000) + 20000
             addiction = true
         }
         self.death = arc4random_uniform(101)
@@ -44,20 +44,24 @@ class life {
     func goToSchool(_ name: String){
         var year = 5
         while year < 17 {
-            if money > 0
+            if money > 0 && self.death > year
             {
                 privateSchool()
                 year += 1
                 print("At age \(year), \(name) went to a private school")
-            } else if money > -10001
+            } else if money > -100001 && self.death > year
             {
                 publicSchool()
                 year += 1
                 print("At age \(year), \(name) went to a public school")
-            } else {
+            } else if self.death > year {
                 childWork()
                 year += 1
                 print("At age \(year), \(name) had the fun time of working as a totally legal child labourer")
+            } else{
+                year = 1000
+                print("At the age of \(self.death) \(name) died")
+                break
             }
         }
         
@@ -72,4 +76,4 @@ print(bob.money)
 bob.goToSchool("bob")
 print(bob.intelligence)
 print(bob.money)
-
+bob.death
